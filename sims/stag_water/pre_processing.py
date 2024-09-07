85c4311b6e69420ce489c89bc64a82e91fefc64b
jhill1 <jon.hill@york.ac.uk>
2024-09-07 17:01:41 +0100

    Adding explicit tidal stag water test

---
+++ sims/stag_water/pre_processing.py
@@ -0,0 +1,88 @@
+from thetis import *
+import hrds
+import sys
+import os.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+import params
+
+mesh2d = Mesh(os.path.join(os.path.pardir,os.path.pardir,params.mesh_file)) # mesh file
+
+viscosity = 1.0 # viscosity, obvs. 1.0 is a decent value. 10 is high, 0.001 is very low. Lower means more 
+# pretty eddies etc, but harder to solve, more likely to crash. Higher is more stable, but less realistic.
+
+manning_drag = 0.025 # which value of drag?
+
+# which boundary is the forced boundary. We increase the visc and manning there to aid stability
+forcing_boundary = params.forcing_boundary
+
+# what distance should be used for the boundary blending (m)
+blend_dist = 50000
+
+# first deal with bathymetry
+with timed_stage('initialising bathymetry'):
+    bathy = hrds.HRDS("../../data/gbr_400_utm56S.tif",rasters=['../../data/gbr_100_utm56S_cropped.tif','../../data/oti_bathy_utm56S_filled_cropped.tif'],distances=[500.0,10.0])
+    bathy.set_bands()
+    P1_2d = FunctionSpace(mesh2d, 'CG', 1)
+    bathymetry2d = Function(P1_2d, name="bathymetry")
+    xvector = mesh2d.coordinates.dat.data
+    bvector = bathymetry2d.dat.data
+    assert xvector.shape[0]==bvector.shape[0]
+    for i, (xy) in enumerate(mesh2d.coordinates.dat.data):
+        bvector[i] = -1.0 * bathy.get_val(xy)
+
+chk = CheckpointFile('bathymetry.h5', 'w')
+chk.save_mesh(mesh2d)
+chk.save_function(bathymetry2d, name='bathymetry')
+
+# now create distance from boundary function
+# typical length scale
+
+PETSc.Sys.Print("Done bathy")
+L = Constant(1.0e3)
+V = FunctionSpace(mesh2d, 'CG', 1)
+v = TestFunction(V)
+u = Function(V)
+u.interpolate(Constant(0.0))
+bcs = DirichletBC(V, Constant(0.0), forcing_boundary) #make sure this matches physicalID of open boundaries
+solver_parameters = {
+    'ksp_rtol': 1e-4,
+}
+# Before we solve the Eikonal equation, let's solve a Laplace equation to
+# generate an initial guess
+F = L**2*(inner(grad(u), grad(v))) * dx - v * dx
+solve(F == 0, u, bcs)#, solver_parameters=solver_parameters)
+solver_parameters = {
+    'ksp_rtol': 1e-4,
+}
+# epss values set the accuracy (in meters) of the final "distance to boundary" function. To make
+# more accurate add in extra iterations, eg, 500., 250., etc. This may result in the solver not
+# converging.
+epss = [100000., 50000., 10000., 7500., 5000., 2500.,1000.]
+# solve Eikonal equations
+for i, eps in enumerate(epss):
+    PETSc.Sys.Print('Solving Eikonal with eps == ' + str(float(eps)))
+    F = inner(sqrt(inner(grad(u), grad(u))), v) * dx - v * dx + eps*inner(grad(u), grad(v)) * dx
+    solve(F == 0, u, bcs, solver_parameters=solver_parameters)
+
+# for checking your blend distance
+dist = Function(V, name='dist')
+dist.interpolate(u)
+File('dist.pvd').write(u)
+
+# create a viscosity buffer
+#create boundary of increased viscosity
+chk = CheckpointFile('viscosity.h5', 'w')
+with timed_stage('initialising viscosity'):
+    h_viscosity = Function(V, name='viscosity')
+    h_viscosity.interpolate(max_value(viscosity, (viscosity*1000) * (1. - u / blend_dist)))
+    chk.save_mesh(mesh2d)
+    chk.save_function(h_viscosity, name='viscosity')
+
+# create a manning drag function
+#create manning boundary of increased bottom friction
+chk = CheckpointFile('manning.h5', 'w')
+with timed_stage('initialising manning'):
+    manning = Function(V, name='manning')
+    manning.interpolate(max_value(manning_drag, (manning_drag*10) * (1. - u / blend_dist)))
+    chk.save_mesh(mesh2d)
+    chk.save_function(manning, name='manning')
---
+++ sims/stag_water/tidal_model.py
@@ -0,0 +1,107 @@
+from thetis import *
+import sys
+import os.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+import params
+import utm
+
+#timestepping options
+dt = 180 # reduce if solver does not converge
+t_export = params.output_time
+t_end = params.end_time
+output_dir = params.output_dir
+utm_zone = params.utm_zone
+utm_band=params.utm_band
+cent_lat = params.cent_lat
+cent_lon = params.cent_lon
+
+# read bathymetry code
+chk = CheckpointFile('bathymetry.h5', 'r')
+mesh2d = chk.load_mesh()
+bathymetry2d = chk.load_function(mesh2d,'bathymetry')
+chk.close()
+
+#read viscosity / manning boundaries code
+chk = CheckpointFile('viscosity.h5', 'r')
+h_viscosity = chk.load_function(mesh2d,'viscosity')
+chk.close()
+chk = CheckpointFile('manning.h5', 'r')
+manning = chk.load_function(mesh2d, 'manning')
+chk.close()
+
+# function to set up the Coriolis force
+# Depends on a "central" lat/lon point in
+# your mesh
+def coriolis(mesh, lat, lon):
+    R = 6371e3
+    Omega = 7.292e-5
+    lat_r = lat * pi / 180.
+    f0 = 2 * Omega * sin(lat_r)
+    beta = (1 / R) * 2 * Omega * cos(lat_r)
+    x = SpatialCoordinate(mesh)
+    x_0, y_0, utm_zone, zone_letter = params.from_latlon(lat, lon)
+    coriolis_2d = Function(FunctionSpace(mesh, 'CG', 1), name="coriolis_2d")
+    coriolis_2d.interpolate(f0 + beta * (x[1] - y_0))
+
+    return coriolis_2d
+
+#account for Coriolis code - mesh, centre lat, centre lon
+coriolis_2d = coriolis(mesh2d, cent_lat, cent_lon)
+
+# --- create solver ---
+solverObj = solver2d.FlowSolver2d(mesh2d, bathymetry2d)
+options = solverObj.options
+options.use_nonlinear_equations = True
+options.simulation_export_time = t_export
+options.simulation_end_time = t_end
+options.output_directory = output_dir
+options.check_volume_conservation_2d = True
+#options.fields_to_export = ['uv_2d', 'elev_2d']
+options.fields_to_export = []
+options.fields_to_export_hdf5 = ['uv_2d', 'elev_2d']
+options.manning_drag_coefficient = manning #the manning function we created in initialisation & loaded above
+options.horizontal_viscosity = h_viscosity #the viscosity 'cushion' we created in initialisation & loaded above
+options.coriolis_frequency = coriolis_2d
+options.timestep = dt
+options.use_automatic_wetting_and_drying_alpha = True
+options.wetting_and_drying_alpha_min = Constant(0.1)
+options.wetting_and_drying_alpha_max = Constant(75.0)
+options.use_wetting_and_drying = True
+options.element_family = "dg-dg"
+options.swe_timestepper_type = 'DIRK22'
+
+
+options.swe_timestepper_options.solver_parameters = {
+      'snes_type': 'newtonls',
+      'snes_rtol': 1e-3,
+      'ksp_rtol': 1e-3,
+      'ksp_type': 'gmres',
+      'pc_type': 'fieldsplit',
+  }
+
+    
+# set boundary/initial conditions code
+tidal_elev = Function(bathymetry2d.function_space())
+solverObj.bnd_functions['shallow_water'] = {
+    #set open boundaries to tidal_elev function
+    params.forcing_boundary: {'un': 0.0},
+    #set closed boundaries to zero velocity
+    1000: {'un': 0.0},
+}
+
+
+solverObj.assign_initial_conditions(uv=Constant((1.0e-12,1.0e-12)))
+
+#work out our coords in lat/lon to save doing this every timestep.
+mesh2d = tidal_elev.function_space().mesh()
+xvector = mesh2d.coordinates.dat.data
+llvector = []
+for i,xy in enumerate(xvector):
+    ll = params.utm.to_latlon(xy[0], xy[1], utm_zone, utm_band)
+    llvector.append(ll)
+
+def update_forcings(t):
+  with timed_stage('update forcings'):
+    pass
+
+solverObj.iterate(update_forcings=update_forcings)
